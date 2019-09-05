053e7ab88ec86e98dd2c877be16d39e73d89e4c6
Walter Bucolo <walter.bucolo@eventbrite.com>
2019-09-05 10:38:25 -0300

    fix url

--- todolist/urls.py
+++ todolist/urls.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 from todolistapp.views import TaskLogin, TaskLogout
 
 urlpatterns = [
-    url('', TaskLogin.as_view(), name='login'),
     url(r'^account/login/$', TaskLogin.as_view(), name='login'),
     url(r'^account/$', TaskLogout.as_view(), name='logout'),
     url(r'^admin/', admin.site.urls),
     url(r'events/', include('todolistapp.urls')),
     url('', include('social_django.urls', namespace='social')),
+    url('', TaskLogin.as_view(), name='login'),
 ]
